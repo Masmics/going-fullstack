5906b77ed699c101728527505bbb04e0e6579ccf
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-12-07 13:31:33 -0800

    added some edits

--- app/src/router.js
+++ app/src/router.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router';
-import Home from './src/components/Home.vue'; //or not src?
-import Nonprofits from './src/components/Nonprofits.vue';
+import Home from './components/Home.vue'; //or not src?
+import Nonprofits from './components/Nonprofits.vue';
+
 
 export default new VueRouter({
   routes: [
